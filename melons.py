760c6d3c57a1219425975008170a9a9fc88dd9f5
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:02:36 -0800

    updated melon info dictionary

---
+++ melons.py
@@ -0,0 +1,13 @@
+melon_info = {
+    'Honeydew': {'price': 0.99, 'seedless': True, 'flesh_color': 0, 'weight': 0,
+                'rind_color': 0},
+    'Crenshaw': {'price': 2.00, 'seedless': False, 'flesh_color': 0, 'weight': 0,
+                'rind_color': 0}
+    'Crane': 'price': 2.50, 'seedless': False, 'flesh_color': 0, 'weight': 0,
+                'rind_color': 0},
+    'Casaba': 'price': 2.50, 'seedless': False, 'flesh_color': 0, 'weight': 0,
+                'rind_color': 0},
+    'Cantaloupe': 'price': 0.99, 'seedless': False, 'flesh_color': 0, 'weight': 0,
+                'rind_color': 0}
+    }
+
